4c9773424ec6a16fc3e1f898a4ed655cd41eac79
Maksym Chapliuk <m.chapliuk@gmail.com>
2016-06-15 12:51:26 +0300

    Gulp settings

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const less = require('gulp-less');
+const livereload = require('gulp-livereload'); // Requires special Chrome extension
+const http = require('http');
+const st = require('st'); //A module for serving static files. Does etags, caching, etc.
+
+const JS_LIST = ['js/**/*.js'];
+const LESS_LIST = ['less/**/*.less'];
+
+gulp.task('less', () => {
+  return gulp.src(LESS_LIST)
+    .pipe(less())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(livereload());
+});
+
+gulp.task('babel', () => {
+  return gulp.src(JS_LIST)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(livereload.reload('dist/index.html'));
+});
+
+gulp.task('server', done => {
+  gulp.src('index.html')
+    .pipe(gulp.dest('dist'));
+  http.createServer(
+    st({
+      path: `${__dirname}/dist`,
+      index: 'index.html',
+      cache: false
+    })
+  ).listen(8080, done);
+});
+
+gulp.task('watch', ['server'], () => {
+  livereload.listen({
+    basePath: 'dist',
+    host: 'localhost'
+  });
+  livereload.reload('dist/index.html');
+  gulp.watch(LESS_LIST, ['less']);
+  gulp.watch(JS_LIST, ['babel']);
+});
---
+++ js/app.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let field = document.getElementById('field');
+});
